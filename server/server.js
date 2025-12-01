4a1462032feca3cccfb6cba33e21c892bb7befaf
Bidipta Roy <bidiptar@bu.edu>
2025-12-01 15:40:07 -0500

    fixed slight mistake in server.js file

--- server/server.js
+++ server/server.js
@@ -33,9 +33,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
-```
-
-### 10. `server/.env`
-```
-PORT=3000
-MONGODB_URI=mongodb://localhost:27017/bjj-trainer
